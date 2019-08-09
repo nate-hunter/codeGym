ff390f8f70d3cd5fde32de6cd86ab966447ec85f
Nate <hunter.j.nate@gmail.com>
2019-08-09 17:53:21 -0400

    task 0526

--- 1.JavaSyntax/src/com/codegym/task/task05/task0526/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0526/Solution.java
@@ -7,8 +7,40 @@ Man and woman
 
 public class Solution {
     public static void main(String[] args) {
-        //write your code here
+        Man man1 = new Man("Panda", 15, "Bamboo Forrest");
+        Man man2 = new Man("Pueo", 56, "Tree in woods");
+
+        Woman woman1 = new Woman("Pineapple", 22, "Patch");
+        Woman woman2 = new Woman("Peach", 33, "Backyard Tree");
+
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
     }
 
-    //write your code here
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
 }

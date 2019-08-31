9a4497b2277a95d101a98225fde87729ce2086f9
Nate <hunter.j.nate@gmail.com>
2019-08-30 23:44:10 -0400

    level 10 java syntax complete

--- 1.JavaSyntax/src/com/codegym/task/task10/task1019/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1019/Solution.java
@@ -18,22 +18,44 @@ public class Solution {
 //        int id = Integer.parseInt(reader.readLine());
 //        String name = reader.readLine();
 
-        String strID = reader.readLine();
-        int id = Integer.parseInt(strID);
-        String name = reader.readLine();
+//        String strID = reader.readLine();
+//        int id = Integer.parseInt(strID);
+//        String name = reader.readLine();
+//
+//        if (strID.length() > 0 && name.length() > 0){
+//            pairs.put(name, id);
+//            int idLoop = Integer.parseInt(reader.readLine());
+//            String nameLoop = reader.readLine();
+//            while (pairs.isEmpty()){
+//                pairs.put(nameLoop, idLoop);
+//                break;
+//            }
+//        }
 
-        if (strID.length() > 0 && name.length() > 0){
-            pairs.put(name, id);
-            int idLoop = Integer.parseInt(reader.readLine());
-            String nameLoop = reader.readLine();
-            while (pairs.isEmpty()){
-                pairs.put(nameLoop, idLoop);
+        while (true){
+            try {
+                String strID = reader.readLine().trim();
+                int id = Integer.parseInt(strID);
+                String name = reader.readLine();
+                if (!(name.length() > 0)) {
+                    pairs.put(" ", id);
+                    break;
+                }
+                pairs.put(name, id);
+            }
+            catch (Exception e){
+//                System.out.println(e);
                 break;
             }
+//            if (strID.length() > 0 && name.length() > 0){
+//                pairs.put(name, id);
+//            } else{
+//                break;
+//            }
         }
 
         for (Map.Entry<String, Integer> e : pairs.entrySet() ){
-            System.out.println(e.getValue() + " " + e.getValue());
+            System.out.println(e.getValue() + " " + e.getKey());
         }
 
 //        System.out.println("Id=" + id + " Name=" + name);
